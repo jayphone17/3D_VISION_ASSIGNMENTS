e63e65e5ffb05746c8c47a831653fdb8c5c00c4e
jayphone17 <m18026629951@163.com>
2021-11-02 11:49:47 +0800

    update Assignment2

---
+++ Assignment_2/build/CMakeFiles/RegionGrowing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RegionGrowing.dir/RegionGrowing.cpp.o"
+  "CMakeFiles/RegionGrowing.dir/RegionGrowing.cpp.o.d"
+  "RegionGrowing"
+  "RegionGrowing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RegionGrowing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Assignment_2/build/CMakeFiles/RegionGrowing.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RegionGrowing.
+# This may be replaced when dependencies are built.
